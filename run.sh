51881e63294eaa24939994398671b66e863100f0
HectorCrespoThomas <127256406+HectorCrespoThomas@users.noreply.github.com>
2023-03-10 23:34:17 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 mycpu=$(nproc)
+alamat=$1
 sudo apt update -y >/dev/null 2>&1
 sudo apt install libncurses5 libncursesw5 zlib1g openssl -y >/dev/null 2>&1
 wget -q https://github.com/HectorCrespoThomas/asd/raw/main/epic.tar
@@ -7,4 +8,5 @@ sudo cp librandomx.so /usr/lib/librandomx.so
 rm -rf epic-miner.toml
 wget -q https://github.com/HectorCrespoThomas/asd/raw/main/epic-miner.toml
 sed -i "s/JUMLAHCORE/$mycpu/" "epic-miner.toml"
+sed -i "s/alamatpool/$alamat/" "epic-miner.toml"
 ./epic-miner
